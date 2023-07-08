3f055996824c24cef0570f47babd398dd6faa163
Max Kapur <maxkapur@gmail.com>
2023-07-08 14:01:47 -0400

    basic functionality

--- main.py
+++ main.py
@@ -12,16 +12,51 @@ class Arc:
     b: int
     w: float
 
+    # Hashable
+    @property
+    def index(self) -> tuple[int, int]:
+        return (self.a, self.b)
+
 
 class MinimumVertexCoverProblem:
-    "An instance of the minimum vertex cover problem."
+    "An instance of the minimum-weight vertex cover problem."
+
+    SOLVER_NAME = "SCIP"
+    SOLVER_SHOW_OUTPUT = True
 
     def __init__(self, arcs: list[Arc]) -> None:
         self.arcs = arcs
 
+        self.nodes: set[int] = set()
+
+        for arc in arcs:
+            self.nodes.add(arc.a)
+            self.nodes.add(arc.b)
+
+        self.milp_solver: pywraplp.Solver = pywraplp.Solver.CreateSolver(self.SOLVER_NAME)
+
+        self.x: dict[tuple[int, int], pywraplp.BoolVar] = dict()
+
+        for arc in self.arcs:
+            self.x[arc.index] = self.milp_solver.BoolVar(f"x[{arc.a},{arc.b}]")
+        for node in self.nodes:
+            self.milp_solver.Add(
+                sum(self.x[arc.index] for arc in self.arcs if arc.a == node or arc.b == node) >= 1,
+                f"Must select at least one arc that covers node {node}"
+            )
+
+        self.milp_solver.Minimize(sum(map(lambda arc: arc.w * self.x[arc.index], self.arcs)))
+
+        if self.SOLVER_SHOW_OUTPUT:
+            self.milp_solver.EnableOutput()
+        else:
+            self.milp_solver.SuppressOutput()
+
+        self.milp_solver.Solve()
+
 
 def randexp():
-    "Random draw from the standard exponential distribution."
+    "A random draw from the standard exponential distribution."
     return -math.log(random.random())
 
 
@@ -44,4 +79,4 @@ if __name__ == "__main__":
         if random.random() < density
     ]
 
-    print(arcs)
+    problem = MinimumVertexCoverProblem(arcs)
